d48b13f08e5a867c537c9ee28a5345c25247db88
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2023-03-07 07:05:33 +0700

    initialization

---
+++ components/Navbar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const menus = [
+  {
+    text: "Home",
+    to: "/",
+  },
+  {
+    text: "About",
+    to: "/about",
+  },
+  {
+    text: "Experience",
+    to: "/experience",
+  },
+  {
+    text: "Education",
+    to: "/education"
+  },
+  {
+    text: "Project",
+    to: "/project",
+    class: "hover:text-sky-700 font-bold"
+  }
+];
+</script>
+
+<style>
+nav {
+  gap: 10px;
+}
+</style>
+
+<template>
+  <VNavbar :menus="menus" class="bg-blue-200"></VNavbar>
+</template>
